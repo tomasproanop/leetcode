4bc68eac33ece5f2b5005b0bcf16ab9eb805e951
tomás <67541868+tomasproanop@users.noreply.github.com>
2023-12-15 23:05:53 +0100

    Add alternative solution

--- sql/article-views-I.sql
+++ sql/article-views-I.sql
@@ -2,3 +2,11 @@ select distinct author_id as id
 from views 
 where author_id = viewer_id
 order by id asc;
+
+# better runtime: 
+
+select author_id as id
+from Views
+where author_id = viewer_id
+group by author_id
+order by author_id asc
