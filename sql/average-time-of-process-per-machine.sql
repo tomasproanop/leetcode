f04cff9341b85bd48ffc1a55e1fdfafea3774002
tomás <67541868+tomasproanop@users.noreply.github.com>
2023-12-16 12:33:27 +0100

    Create average-time-of-process-per-machine.sql

---
+++ sql/average-time-of-process-per-machine.sql
@@ -0,0 +1,8 @@
+select s.machine_id, round (avg(e.timestamp-s.timestamp), 3) as processing_time
+from activity s inner join activity e
+    on s.machine_id = e.machine_id
+    and s.process_id = e.process_id
+    and s.activity_type = 'start'
+    and e.activity_type = 'end'
+group by s.machine_id
+order by null;
